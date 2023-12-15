3a1b6e9354c6cf548eb85523009a56fc0729f5bf
Francesco Russo <franci.russo@icloud.com>
2023-12-15 12:53:06 +0100

    scheda

--- js/scheda.js
+++ js/scheda.js
@@ -17,42 +17,65 @@ function bindEvents() {
 }
 
 function visualizzaSchede(){
-    req=new XMLHttpRequest();
+    req = new XMLHttpRequest();
 
-    req.onload=function(){
-        if(req.readyState==4 && req.status==200){
-           var data = JSON.parse(req.responseText);
+    req.onload = function(){
+        if(req.readyState == 4 && req.status == 200){
+            var data = JSON.parse(req.responseText);
 
-           console.log(data);
-           data.forEach(scheda => {
+            console.log(data);
+
+            // Itera su ogni scheda ricevuta dalla risposta JSON
+            data.forEach(scheda => {
                 var div;
-                if(scheda.data_fine!=null){
+                // Determina se la scheda è nel div dello storico o nell'attuale
+                if(scheda.data_fine != null){
                     div = storico_schede;
-                }
-                else{
+                } else {
                     div = scheda_attuale;
                 }
+                // Crea un elemento div per la card e lo aggiunge al div corretto
                 const card = document.createElement("div");
-                card.setAttribute("class","card mt-2 span");
+                card.setAttribute("class", "card mt-2 span");
                 div.appendChild(card);
+                // Crea un elemento div per il corpo della card e lo aggiunge alla card
                 const card_body = document.createElement("div");
-                card_body.setAttribute("class","card-body");
+                card_body.setAttribute("class", "card-body");
                 card.appendChild(card_body);
+                
+                // Creazione dell'elemento 'a' per il link
+                const link = document.createElement("a");
+                link.href = "pagina_scheda.php?id=" + scheda.id_scheda; // URL con il parametro ID
+                link.style.textDecoration = "none"; // Rimuovi la sottolineatura
+                card_body.appendChild(link);
+
                 const h3 = document.createElement("h3");
-                card_body.appendChild(h3);
+                link.appendChild(h3);
 
-                /*var botton_chiusura=document.createElement("button");
-                botton_chiusura.classList("btn btn-primary");
-                */
-
-
-
-                //const d = new Date(scheda.data_inizio);
-                //console.log(d);
+                const hiddenInput = document.createElement('input');
+                hiddenInput.type = 'hidden';
+                hiddenInput.name = 'id_scheda'; // Nome del campo per identificarlo
+                hiddenInput.value = scheda.id_scheda;
+                hiddenInput.id = scheda.id_scheda;
+                card_body.appendChild(hiddenInput);
+            
+            
                 h3.innerHTML = "Scheda del "+scheda.data_inizio;
-           });
+                
+                // Se la scheda è nell'attuale, crea e aggiunge un bottone "Termina la scheda"
+                if(div == scheda_attuale){
+                    var button_chiusura = document.createElement("button");
+                    button_chiusura.classList = "btn btn-primary";
+                    button_chiusura.textContent = "Termina la scheda ";
+                    button_chiusura.setAttribute("id", "button_chiusura");
+                    button_chiusura.style.float = "right";
+                    card_body.appendChild(button_chiusura);
+                }
+            });
         }
     }
+
+
 
     req.open("GET", "php/logicaSchede.php/schede/storico", true);
     req.send();
